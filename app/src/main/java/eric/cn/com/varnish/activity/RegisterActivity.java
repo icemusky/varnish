73df1350b26e14731d43beeeca31b3754bf37c22
Eric <icemusky@126.com>
2017-12-03 22:00:40 +0800

    增加 注册 个人信息

--- app/src/main/java/eric/cn/com/varnish/activity/RegisterActivity.java
+++ app/src/main/java/eric/cn/com/varnish/activity/RegisterActivity.java
@@ -3,6 +3,8 @@ package eric.cn.com.varnish.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import eric.cn.com.varnish.R;
 
@@ -10,10 +12,28 @@ import eric.cn.com.varnish.R;
  * Created by Administrator on 2017/11/29.
  */
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+    private LinearLayout ll_balck;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        initView();
+    }
+
+    private void initView() {
+        ll_balck = (LinearLayout) findViewById(R.id.ll_balck);
+
+        ll_balck.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ll_balck:
+                finish();
+                break;
+        }
     }
 }
